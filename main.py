4841d67831948137f5473dd63c942e00a7c9bb4d
YargonIV <fabien.da-costa-barros@etu.univ-grenoble-alpes.fr>
2022-09-30 17:59:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ features = data.exportMeshesData()
 data.plotFeatures(features,['Category'],26,25,10)
 data.plotFeatures(features,['Face numbers', 'Vertex numbers', 'Bounding Box Diagonal Size'])
 
-if (len(sys.argv) == 2):
-    print("Rendering : " + sys.argv[1])
-    rd.render(sys.argv[1])
+#if (len(sys.argv) == 2):
+#    print("Rendering : " + sys.argv[1])
+#    rd.render(sys.argv[1])
