a888fd5a4f61d031d52c723686d4bd27e56673d6
soheil bagheri <soheil.baghery@gmail.com>
2022-10-06 00:51:24 +0200

    remesh and plot hist

--- remesh.py
+++ remesh.py
@@ -1,16 +1,17 @@
 import pymeshlab
 import os
-
+import pandas as pd
 
 pricetonPath = "./Models/PRINCETON/test"
 LabeledPath = r"D:\My Projects\UU\LabeledDB_new"
-
-beforeRemesh = []
-afterRemesh = []
-nErrors = 0
+OutPath = r"D:\My Projects\UU\Remeshed"
+remeshedDf = pd.read_csv(os.path.join(OutPath,"nVertices.csv"))
 
 for dir in os.scandir(LabeledPath):
-    print(dir.name)
+    if (dir.name == remeshedDf["class"]).any():
+        continue
+    curPath = os.path.join(OutPath,dir.name)
+    os.mkdir(curPath)
     FileIt =os.scandir(dir)
     for file in FileIt:
         rPath = os.path.realpath(file)
@@ -21,18 +22,24 @@ for dir in os.scandir(LabeledPath):
                 ms = pymeshlab.MeshSet()
                 ms.load_new_mesh(rPath)
                 cur = ms.current_mesh()
-                print("Face number:" + str(cur.face_number()) + " - Vertex number:" + str(cur.vertex_number()))
-                beforeRemesh.append(cur.vertex_number())
+                beforeRemeshVerices = cur.vertex_number()
                 try:
+                    ms.apply_filter('meshing_remove_duplicate_faces')
+                    ms.apply_filter('meshing_remove_duplicate_vertices')
+                    ms.apply_filter('meshing_remove_unreferenced_vertices')
+                    try :
+                        ms.apply_filter('meshing_repair_non_manifold_edges', method='Split Vertices')
+                        ms.apply_filter('meshing_repair_non_manifold_vertices')
+                        ms.apply_filter('meshing_re_orient_faces_coherentely')
+                    except:
+                        print('ERROR : Mesh has some not 2-manifold faces')
                     ms.compute_iso_parametrization()
                     ms.generate_iso_parametrization_remeshing(samplingrate = 5)
                     cur = ms.current_mesh()
-                    print("Face number:" + str(cur.face_number()) + " - Vertex number:" + str(cur.vertex_number()))
-                    afterRemesh.append(cur.vertex_number())
+                    afterRemeshVerices = cur.vertex_number()
+                    ms.save_current_mesh(os.path.join(curPath, file.name))
                 except:
-                    nErrors += 1
-                    afterRemesh.append("Error")
-
-print (beforeRemesh)
-print (afterRemesh)
-print ("Number of errors:" + str(nErrors))
+                    afterRemeshVerices = "Error"
+                df = pd.DataFrame([[dir.name, file.name, beforeRemeshVerices, afterRemeshVerices]],columns=["class","fileName","beforeRemesh","afterRemesh"])
+                df.to_csv(os.path.join(OutPath,"nVertices.csv"),mode='a')
+                ms.clear()
---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+OutPath = r"D:\My Projects\UU\Remeshed"
+remeshedDf = pd.read_csv(os.path.join(OutPath,"nVertices.csv"))
+df = pd.DataFrame(remeshedDf,columns=["index","class","fileName","beforeRemesh","afterRemesh"])
+fig, axs = plt.subplots(1, 1,figsize=(10, 7),tight_layout=True)
+plt.xlabel("nOfObjects")
+plt.ylabel("nOfVerices")
+plt.title('Numbers of verices after remeshing')
+axs.hist(df["afterRemesh"], bins=10)
+plt.savefig(os.path.join(OutPath,"afterRemesh.png"))
